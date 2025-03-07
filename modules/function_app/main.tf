cd376c9aeaf677818f0e5c139658f125a90c0cd4
chrisevans72 <59256982+chrisevans72@users.noreply.github.com>
2025-03-07 11:37:28 +0000

    for_each value

--- modules/function_app/main.tf
+++ modules/function_app/main.tf
@@ -1,12 +1,12 @@
 resource "azurerm_windows_function_app" "new" {
   for_each = var.functions
 
-  name                        = "myfunc${each.key}"
-  location                    = var.resource_group.location
-  resource_group_name         = var.resource_group.name
-  service_plan_id             = azurerm_service_plan.func_service_plan[each.key].id
-  storage_account_name        = azurerm_storage_account.func_storage[each.key].name
-  storage_account_access_key  = azurerm_storage_account.func_storage[each.key].primary_access_key
+for_each name                        = "myfunc${each.key}"
+for_each location                    = var.resource_group.location
+for_each  resource_group_name         = var.resource_group.name
+for_each  service_plan_id             = azurerm_service_plan.func_service_plan[each.key].id
+for_each  storage_account_name        = azurerm_storage_account.func_storage[each.key].name
+for_each  storage_account_access_key  = azurerm_storage_account.func_storage[each.key].primary_access_key
   https_only                  = true
   client_certificate_mode     = "Required"
   functions_extension_version = "~4"
