e76602ac1f41004c7d8cbb1c79c3679dabb08ed9
chrisevans72 <59256982+chrisevans72@users.noreply.github.com>
2025-03-07 11:45:37 +0000

    amend

--- modules/function_app/main.tf
+++ modules/function_app/main.tf
@@ -1,31 +1,32 @@
 resource "azurerm_windows_function_app" "new" {
   for_each = var.functions
 
-for_each name                        = "myfunc${each.key}"
-for_each location                    = var.resource_group.location
-for_each  resource_group_name         = var.resource_group.name
-for_each  service_plan_id             = azurerm_service_plan.func_service_plan[each.key].id
-for_each  storage_account_name        = azurerm_storage_account.func_storage[each.key].name
-for_each  storage_account_access_key  = azurerm_storage_account.func_storage[each.key].primary_access_key
+  name                        = "myfunc${each.key}"
+  location                    = var.resource_group.location
+  resource_group_name         = var.resource_group.name
+  service_plan_id             = azurerm_service_plan.func_service_plan[each.key].id
+  storage_account_name        = azurerm_storage_account.func_storage[each.key].name
+  storage_account_access_key  = azurerm_storage_account.func_storage[each.key].primary_access_key
+  
   https_only                  = true
   client_certificate_mode     = "Required"
-  functions_extension_version = "~4"
+  functions_extension_version  = "~4"
 
   site_config {
-    ftps_state = "Disabled"
-    app_scale_limit = 2
-    use_32_bit_worker = false
+    ftps_state         = "Disabled"
+    app_scale_limit    = 2
+    use_32_bit_worker  = false
 
     application_stack {
-      dotnet_version  = "v8.0"
-      use_dotnet_isolated_runtime = true
+      dotnet_version                    = "v8.0"
+      use_dotnet_isolated_runtime       = true
     }
   }
 
   app_settings = {
-      "WEBSITE_RUN_FROM_PACKAGE" = "1"
-      "environmentApplicationConfig" = var.app_config_uri
-    }
+    "WEBSITE_RUN_FROM_PACKAGE"       = "1"  # Ensure this setting is what you intend
+    "environmentApplicationConfig"    = var.app_config_uri
+  }
 
   tags = var.tags
 
